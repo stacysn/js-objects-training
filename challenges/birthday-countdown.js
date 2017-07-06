9da841cc8962c018dea1269a1fe17c807f39f778
YOUR_GITHUB_USERNAME <stacysuen@Stacy-Stace.attlocal.net>
2017-07-05 22:30:35 -0700

    added birthday but still counting days in years

--- challenges/birthday-countdown.js
+++ challenges/birthday-countdown.js
@@ -26,11 +26,28 @@
   //      "Jill's birthday is in 305 days"
   //    ]
   ```
-
   Bonuses
   - Will your function still work tomorrow when the date is different? Is it future proofed?
   - To make your output more relevant, can you sort by days remaining (ascending)?
-
 */
 
 // YOUR CODE HERE
+function daysUntilDate(date){
+   var oneDay = 1000 * 60 * 60 * 24;
+   var splitDate = date.split("/");
+   var today = new Date ();
+   var dd = today.getDate();
+   var mm = today.getMonth() + 1;
+   var yyyy = today.getFullYear();
+   var todayDate = (mm + "/" + dd + "/" + yyyy);
+
+   var splitToday = todayDate.split("/");
+
+   var theDate1 = new Date(splitDate[2], splitDate[0], (splitDate[1] - 1));
+   var newToday = new Date(splitToday[2], splitToday[0], (splitToday[1] - 1));
+
+   var diff = Math.ceil((newToday.getTime() - theDate1.getTime())/(oneDay));
+   console.log(diff);
+}
+
+daysUntilDate('07/01/2017'); //NEED YEAR
